afa69dbddc9f60c75eef084b6de9b053c18ff1bc
David <david.thornqvist@elev.ga.ntig.se>
2026-05-07 09:23:05 +0200

    Added update to server.js and attempted to add delete to server.js unsure why it does not wwork at the moment

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const sqlite3 = require("sqlite3");
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 
 const db = new sqlite3.Database("./tasks.db");
+
 db.run(
   `CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT NOT NULL, completed BOOLEAN DEFAULT FALSE)`,
 );
@@ -12,6 +14,7 @@ db.run(
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
+
 app.get("/tasks", (req, res) => {
   db.all("SELECT * FROM tasks", (error, rows) => {
     res.json(rows);
@@ -21,6 +24,7 @@ app.get("/tasks", (req, res) => {
 app.post("/tasks", (req, res) => {
   const title = req.body.title;
   const completed = req.body.completed;
+
   db.run(
     "INSERT INTO tasks (title, completed) VALUES (?, ?)",
     [title, completed],
@@ -33,6 +37,34 @@ app.post("/tasks", (req, res) => {
   );
 
   res.status(201).json({ message: "Task created" });
+});
+app.put("/tasks/:id", (req, res) => {
+  const completed = req.body.completed;
+  const id = req.params.id;
+  const title = req.body.title;
+
+  db.run(
+    "UPDATE tasks SET title = ?, completed = ? WHERE id = ?",
+    [title, completed, id],
+    (error) => {
+      if (error) {
+        console.log(error.code);
+        return res.status(500).json({ error: "DB error" });
+      }
+    },
+  );
+  res.status(201).json({ message: "Task updated" });
+});
+app.delete("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.run("DELETE FROM tasks WHERE id = ?", [id], (error) => {
+    if (error) {
+      console.log(error.code);
+      return res.status(500).json({ error: "DB error" });
+    }
+    res.status(200).json({ message: "Task deleted" });
+  });
 });
 
 app.listen(PORT, () => {
